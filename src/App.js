fc1b6b474acca60db6cd268b74f191b4fca2d9dd
Mohamed Talaat <mohamedtalaat333@gmail.com>
2023-05-18 16:03:29 +0300

    latest

--- src/App.js
+++ src/App.js
@@ -4,20 +4,11 @@ import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+  function fetchMoviesHandler() {
+    fetch("https://swapi.dev/api/films/").then((res) => console.log(res)).catch(() => console.log("error"));
+  }
+  
+  fetchMoviesHandler();
 
   return (
     <React.Fragment>
@@ -25,7 +16,7 @@ function App() {
         <button>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={dummyMovies} />
+        {/* <MoviesList movies={dummyMovies} /> */}
       </section>
     </React.Fragment>
   );
